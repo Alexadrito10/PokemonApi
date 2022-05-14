56135fab9111470b348002f7abd7dad2cd195821
Fernanda <45322807+FernandaRojas152@users.noreply.github.com>
2022-05-14 18:45:29 -0500

    Creating the model panel and changing some UI

---
+++ app/src/main/java/co/icesi/edu/pokemonapi/model/Pokemon.kt
@@ -0,0 +1,16 @@
+package co.icesi.edu.pokemonapi.model
+
+import java.io.Serializable
+
+data class Pokemon(
+    val id: String ="",
+    val name: String ="",
+    val image:String="",
+    val username: String="",
+    val health:String="",
+    val attack: String="",
+    val defense: String="",
+    val speed: String="",
+    val type: String="",
+    val date: Long=0
+):Serializable
---
+++ app/src/main/java/co/icesi/edu/pokemonapi/model/User.kt
@@ -0,0 +1,7 @@
+package co.icesi.edu.pokemonapi.model
+
+import java.io.Serializable
+
+data class User(
+    val username:String=""
+):Serializable
