1ef0eef8ccfc3e0edf48a01ef3d95fcc16b3fda3
Alex Samacá <alexadrito10@gmail.com>
2022-05-14 13:49:14 -0500

    Initial commit

---
+++ app/src/main/java/co/icesi/edu/pokemonapi/dex.kt
@@ -0,0 +1,11 @@
+package co.icesi.edu.pokemonapi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class dex : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dex)
+    }
+}
