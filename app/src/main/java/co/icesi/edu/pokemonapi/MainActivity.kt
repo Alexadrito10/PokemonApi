b66ca38207748e3176bf77182de9262cae7d1a2d
Alex Samacá <alexadrito10@gmail.com>
2022-05-15 12:51:05 -0500

    Update MainActivity.kt

--- app/src/main/java/co/icesi/edu/pokemonapi/MainActivity.kt
+++ app/src/main/java/co/icesi/edu/pokemonapi/MainActivity.kt
@@ -21,32 +21,9 @@ class MainActivity : AppCompatActivity() {
         supportActionBar?.hide()
 
         setContentView(binding.root)
-        binding.login.setOnClickListener {
-            var username= binding.username.text.toString()
-            username= username.trim()
-
-            if(username!= ""){
-                //Creando el intent para los dos casos e ir a la pagina donde se atrapa el pokemon
-                val intent = Intent(this, dex::class.java).apply{
-                    putExtra("username", username)
-                }
-                Firebase.firestore.collection("users").whereEqualTo("username", username)
-                    .get().addOnCompleteListener { task ->
-
-                        if(task.result?.size()==0){
-                            Firebase.firestore.collection("users").add(User(username))
-                            startActivity(intent)
-                        }else{
-                            startActivity(intent)
-                        }
-                    }
-            }else{
-                Toast.makeText(this, "Empty username", Toast.LENGTH_SHORT).show()
-            }
-        }
-        //binding.login.setOnClickListener(::connect)
+        binding.login.setOnClickListener(::connect)
     }
-    /**private fun connect(view: View){
+    private fun connect(view: View){
         var username= binding.username.text.toString()
         username= username.trim()
 
@@ -69,5 +46,4 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this, "Empty username", Toast.LENGTH_SHORT).show()
         }
     }
-    */
 }
