b772144daec66021bd9408f5b375de5ecfdacd7e
Fernanda <45322807+FernandaRojas152@users.noreply.github.com>
2022-05-14 21:30:39 -0500

    adding adapter

---
+++ app/src/main/java/co/icesi/edu/pokemonapi/PokemonViewHolder.kt
@@ -0,0 +1,11 @@
+package co.icesi.edu.pokemonapi
+
+import android.view.View
+import androidx.appcompat.view.menu.ActionMenuItemView
+import androidx.recyclerview.widget.RecyclerView
+
+class PokemonViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+    init{
+
+    }
+}
---
+++ app/src/main/java/co/icesi/edu/pokemonapi/adapter/PokemonAdapter.kt
@@ -0,0 +1,26 @@
+package co.icesi.edu.pokemonapi.adapter
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import co.icesi.edu.pokemonapi.PokemonViewHolder
+import co.icesi.edu.pokemonapi.model.Pokemon
+
+class PokemonAdapter : RecyclerView.Adapter<PokemonViewHolder>() {
+    private var pokemones= ArrayList<Pokemon>()
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PokemonViewHolder {
+        TODO("Not yet implemented")
+    }
+
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: PokemonViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+
+}
