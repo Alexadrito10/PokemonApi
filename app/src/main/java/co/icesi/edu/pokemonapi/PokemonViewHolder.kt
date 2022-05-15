82cce86e3189dad2949ec2585a0000ef87d25623
Fernanda <45322807+FernandaRojas152@users.noreply.github.com>
2022-05-14 21:55:51 -0500

    Viewholder finished

--- app/src/main/java/co/icesi/edu/pokemonapi/PokemonViewHolder.kt
+++ app/src/main/java/co/icesi/edu/pokemonapi/PokemonViewHolder.kt
@@ -1,11 +1,19 @@
 package co.icesi.edu.pokemonapi
 
 import android.view.View
+import android.widget.ImageView
 import androidx.appcompat.view.menu.ActionMenuItemView
 import androidx.recyclerview.widget.RecyclerView
+import co.icesi.edu.pokemonapi.adapter.PokemonAdapter
 
-class PokemonViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+class PokemonViewHolder(itemView: View, listener: PokemonAdapter.ClickPokemon) : RecyclerView.ViewHolder(itemView){
+    var pokemonImage: ImageView = itemView.findViewById(R.id.pokemonImage)
+    var pokemonName: ImageView= itemView.findViewById(R.id.pokemonName)
+    var pokemonDate: ImageView= itemView.findViewById(R.id.catchDate)
+
     init{
-
+        itemView.setOnClickListener {
+            listener.onItemClick(adapterPosition)
+        }
     }
 }
--- app/src/main/java/co/icesi/edu/pokemonapi/adapter/PokemonAdapter.kt
+++ app/src/main/java/co/icesi/edu/pokemonapi/adapter/PokemonAdapter.kt
@@ -1,16 +1,24 @@
 package co.icesi.edu.pokemonapi.adapter
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import co.icesi.edu.pokemonapi.PokemonViewHolder
+import co.icesi.edu.pokemonapi.R
 import co.icesi.edu.pokemonapi.model.Pokemon
+import co.icesi.edu.pokemonapi.model.User
 
 class PokemonAdapter : RecyclerView.Adapter<PokemonViewHolder>() {
     private var pokemones= ArrayList<Pokemon>()
-
+    private lateinit var user: User
+    private lateinit var lis2 : ClickPokemon
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PokemonViewHolder {
-        TODO("Not yet implemented")
+        val inflater= LayoutInflater.from(parent.context)
+        val row= inflater.inflate(R.layout.pokemonrow, parent, false)
+        val pokeView = PokemonViewHolder(row, lis2)
+
+        return pokeView
     }
 
 
@@ -20,6 +28,14 @@ class PokemonAdapter : RecyclerView.Adapter<PokemonViewHolder>() {
 
     override fun onBindViewHolder(holder: PokemonViewHolder, position: Int) {
         TODO("Not yet implemented")
+    }
+
+    fun setOnClickListener(listener: ClickPokemon){
+        lis2 = listener
+    }
+
+    interface ClickPokemon{
+        fun onItemClick(position: Int)
     }
 
 
